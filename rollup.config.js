d67a823082990b160421e36cbb66b90afdd8bb9e
Vlad Savitskyi <vsavytc@gmail.com>
2020-12-15 11:36:51 +0200

    chore: set up typescript

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess';
 import svelteSVG from "rollup-plugin-svelte-svg";
@@ -39,7 +40,7 @@ function serve() {
 }
 
 export default {
-	input: 'src/main.js',
+	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
 		format: 'iife',
@@ -48,6 +49,7 @@ export default {
 	},
 	plugins: [
 		svelte({
+			preprocess: sveltePreprocess(),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
@@ -71,6 +73,10 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
+		typescript({
+			sourceMap: !production,
+			inlineSources: !production
+		}),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Metronome from './metronome/metronome.svelte';
 </script>
 
